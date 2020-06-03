966e247d4c3e6d496ce9e1e89ef480da68e7ea1c
mateuszz0000 <mtszzwdzk@gmail.com>
2020-06-04 00:28:42 +0200

    Added initial asyncio CLI

--- corpobot/main.py
+++ corpobot/main.py
@@ -1,9 +1,30 @@
 import sys
+import aioconsole
+import asyncio
 
 CORPOBOT_VERION = "0.1"
 
+
+async def get_input():
+    while True:
+        _input = await aioconsole.ainput(prompt="Type command or `help` for list commands\n")
+
+        if _input == "help":
+            await print_help()
+        else:
+            print(f"There is no command with name: {_input}")
+            await print_help()
+
+
+async def print_help():
+    print(f"Following commands are available: get_tasks")
+
+
 def main():
-    print (f"Welcome to CorpoBot ! v.{CORPOBOT_VERION}")
+    print(f"Welcome to CorpoBot ! v.{CORPOBOT_VERION}")
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get_input())
+    loop.close()
 
 
 if __name__ == "__main__":
